6337aa3d21b4cf8934ab279175a97f5f4282a002
Hugo Orielso <orielso.lozano15@gmail.com>
2025-05-23 11:56:43 -0500

    feat: :sparkles: Added script to resize heigth of the content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { Toaster } from 'sonner'
 import './App.css'
 import TestForm from './components/TestForm'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    function enviarAltura() {
+      const altura = document.documentElement.scrollHeight
+      window.parent.postMessage({ alturaIframe: altura }, "*")
+    }
 
+    enviarAltura()
+
+    const mutationObserver = new MutationObserver(enviarAltura)
+    mutationObserver.observe(document.body, { childList: true, subtree: true })
+
+    const resizeObserver = new ResizeObserver(enviarAltura)
+    resizeObserver.observe(document.documentElement)
+
+    window.addEventListener('resize', enviarAltura)
+
+    return () => {
+      mutationObserver.disconnect()
+      resizeObserver.disconnect()
+      window.removeEventListener('resize', enviarAltura)
+    }
+  }, [])
   return (
     <div className='p-2'>
-    
-      <TestForm/>
-      <Toaster richColors/>
+      <TestForm />
+      <Toaster richColors />
     </div>
   )
 }
